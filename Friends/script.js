65c8694e9f7aed4ad8a2bce872db0be4cb13cf71
anafortuna <alobofortuna@gmail.com>
2024-09-30 00:21:23 -0300

    Adição do vídeo na página

--- Friends/script.js
+++ Friends/script.js
@@ -1,9 +1,5 @@
 document.getElementById('botao-principal').addEventListener('click', function() {
-    // Toca o áudio
-    let audio = document.getElementById('audio');
-    audio.play();
-
-    // Abre uma nova página
+    
     window.open('./index2.html', '_blank');
 
  
